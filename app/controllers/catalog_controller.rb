a6fc808b6369c18bf058d9ac61ace145e2bb64b9
Michael Holloway <michael@mdholloway.org>
2025-01-13 23:54:59 -0500

    Remove commented-out code re: earliest/latest_int

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -112,25 +112,6 @@ class CatalogController < ApplicationController
     config.add_facet_field 'artist_facet', label: 'Artist', limit: 5
     config.add_facet_field 'place_facet', label: 'Place', limit: 5 # , single: true
     config.add_facet_field 'century_int', label: 'Century', limit: 5, sort: 'alpha', helper_method: :century_label
-
-    # TESTING IMPLEMENTATION OF INTEGER DATE FIELDS AND BLACKLIGHT_RANGE_LIMIT
-    # config.add_facet_field 'century_facet', label: 'Century', limit:5, sort:'alpha'
-    # config.add_facet_field 'earliest_int', label: 'Earliest Date', limit:5
-    # config.add_facet_field 'latest_int', label: 'Latest Date', limit:5
-
-    # config.add_facet_field 'earliest_int', label: 'Date (Earliest)', range: {
-    #  num_segments:10,
-    #        assumed_boundaries: [800,1700],
-    #        segments: true,
-    #        maxlength: 4
-    #      }, collapse:false
-    # config.add_facet_field 'latest_int', label: 'Date (Latest)', range: {
-    #   num_segments:10,
-    #        assumed_boundaries: [800,1700],
-    #        segments: true,
-    #        maxlength: 4
-    #      }, collapse:false
-
     config.add_facet_field 'language_facet', label: 'Language', limit: 5
     config.add_facet_field 'material_facet', label: 'Material', limit: 5
     config.add_facet_field 'owner_facet', label: 'Former Owner', limit: 5
